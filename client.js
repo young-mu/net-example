9c109a1231c9c762a9cbd85f4d3f3dad2888b450
young <young.mwy@gmail.com>
2017-02-21 20:22:43 +0800

    Update the examples

--- client.js
+++ client.js
@@ -5,23 +5,24 @@ var PORT = 2222;
 
 var client = net.createConnection({port: PORT, host: HOST});
 
-client.on('connect', function() {
-    console.log('CONNECT TO: ' + HOST + ':' + PORT);
+client.on('connect', function () {
+    console.log('> connect to : ' + HOST + ':' + PORT);
     client.write('I\'m Client!');
 });
 
-client.on('data', function(buffer) {
-    var data = buffer.toString();
-    console.log('DATA: ' + data);
-    if (data.indexOf('end') !== -1) {
-        client.destroy();
-    }
+client.on('data', function (buffer) {
+    console.log('> recv: ' + buffer.toString());
+    client.destroy(); // client close actively
 });
 
-client.on('close', function() {
-    console.log('CLOSE CONNECTION');
+client.on('close', function () {
+    console.log('> close connection');
 });
 
-client.on('error', function(error) {
-    console.log('ERROR: ' + error);
+client.on('end', function () {
+    console.log('> ended by server');
+});
+
+client.on('error', function (error) {
+    console.log('> error: ' + error);
 });
--- server.js
+++ server.js
@@ -5,27 +5,28 @@ var PORT = 2222;
 var server = net.createServer();
 server.listen(PORT);
 
-server.on('connection', function(sock) {
-    server.close(function() {
-        console.log('connection is closed ...');
-    });
+server.on('connection', function (sock) {
+    console.log('\n> connected from: ' + sock.remoteAddress + ':' + sock.remotePort);
 
-    console.log('CONNECTED FROM: ' + sock.remoteAddress + ':' + sock.remotePort);
-
-    sock.on('data', function(data) {
-        console.log('DATA: ' + data);
+    sock.on('data', function (data) {
+        console.log('> recv: ' + data);
         sock.write('I\'m Server!');
+//        sock.end(); // server close actively
     });
 
-    sock.on('close', function(data) {
-        console.log('CLOSED BY: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sock.on('end', function () {
+        console.log('> ended by client');
+    });
+
+    sock.on('close', function (data) {
+        console.log('> closed by: ' + sock.remoteAddress + ':' + sock.remotePort);
     });
 });
 
-server.on('listening', function() {
-    console.log('Server starts to listen on ' + '*' +':'+ PORT);
+server.on('listening', function () {
+    console.log('> server starts to listen on ' + '*' +':'+ PORT);
 });
 
-server.on('close', function() {
-    console.log('server is closed ...');
+server.on('close', function () {
+    console.log('> server is closed ...');
 });
